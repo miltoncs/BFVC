319f36d3cf7741707a32f952723a17d60645042a
Milton Stafford <mstafford2012@my.fit.edu>
2018-06-03 23:31:31 -0400

    add scripts

---
+++ scripts/refresh_bfvc.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd /home/ubuntu/BFVC/
+if [[ $(git pull) = *"up-to-date"* ]]; then
+    echo "No code changes, update complete"
+else
+    echo "Code based changed!..."
+    cd ..
+    rm -r /var/www/html/*
+    cp -r BFVC/Ion/_site/* /var/www/html/
+fi
